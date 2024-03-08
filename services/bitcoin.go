32931663ccf44c93cf4ab778419da200f43e2bc7
DESKTOP-QDHV7HA\김찬호 <kispi@naver.com>
2024-03-08 22:41:25 +0900

    fix: add field 'descriptions'

--- services/bitcoin.go
+++ services/bitcoin.go
@@ -25,14 +25,15 @@ type BitcoinService struct{}
 var priceData []*PriceRow
 
 type QuoteRow struct {
-	Name   string `json:"name"`
-	Quotes []struct {
+	Name         string `json:"name"`
+	Descriptions []string `json:"descriptions"`
+	Quotes       []struct {
 		Text   string `json:"text"`
 		Source string `json:"source"`
 		Date   string `json:"date"`
 	} `json:"quotes"`
-	Images []string `json:"images"`
-	Take   string   `json:"take"`
+	Images       []string `json:"images"`
+	Take         string   `json:"take"`
 }
 
 var quotesData []*QuoteRow
